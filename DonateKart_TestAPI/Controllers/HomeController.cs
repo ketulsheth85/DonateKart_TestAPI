ec2f716ebd87ff8afd51da734bbc89bca1a6869a
MonilVaywala <Monil.Vaywala@techcronus.com>
2021-11-25 11:35:59 +0530

    Project Added

---
+++ DonateKart_TestAPI/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using DonateKart_TestAPI.Models;
+
+namespace DonateKart_TestAPI.Controllers
+{
+    [ApiController]
+    [Route("Home")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetListofCampaigns")]
+        public async Task<List<ResponseCampaignDetail>> GetListofCampaigns()
+        {
+            List<ResponseCampaignDetail> campaignlist = new List<ResponseCampaignDetail>();
+            var responseBody = await GetList();
+            var response = JsonConvert.DeserializeObject<List<ResponseCampaignDetail>>(responseBody);
+            return response.OrderByDescending(x => x.totalAmount).Select(a => new ResponseCampaignDetail
+            {
+                title = a.title,
+                totalAmount = a.totalAmount,
+                backersCount = a.backersCount,
+                endDate = a.endDate
+            }).ToList();
+        }
+        
+
+        [HttpGet]
+        [Route("GetListofActiveCampaigns")]
+        public async Task<List<ResponseCampaignDetail>> GetListofActiveCampaigns()
+        {
+            List<ResponseCampaignDetail> campaignlist = new List<ResponseCampaignDetail>();
+            var responseBody = await GetList();
+            var response = JsonConvert.DeserializeObject<List<CampaignDetail>>(responseBody);
+            return response.Where(x => x.endDate >= DateTime.Now.Date && (x.created <= DateTime.Now.Date && x.created >= DateTime.Now.Date.AddDays(-30))).Select(a => new ResponseCampaignDetail
+            {
+                title = a.title,
+                totalAmount = a.totalAmount,
+                backersCount = a.backersCount,
+                endDate = a.endDate
+            }).ToList();
+        }
+
+        [HttpGet]
+        [Route("GetListofClosedCampaigns")]
+        public async Task<List<ResponseCampaignDetail>> GetListofClosedCampaigns()
+        {
+            List<ResponseCampaignDetail> campaignlist = new List<ResponseCampaignDetail>();
+            var responseBody = await GetList();
+            var response = JsonConvert.DeserializeObject<List<CampaignDetail>>(responseBody);
+            return response.Where(x => x.endDate < DateTime.Now.Date || x.procuredAmount >= x.totalAmount).Select(x => new ResponseCampaignDetail
+            {
+                title = x.title,
+                totalAmount = x.totalAmount,
+                backersCount = x.backersCount,
+                endDate = x.endDate
+            }).ToList();
+        }
+
+        [NonAction]
+        public async Task<string> GetList()
+        {
+            string responseString = string.Empty;
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            using (HttpClient client = new HttpClient(clientHandler))
+            {
+                client.BaseAddress = new Uri("https://testapi.donatekart.com/");
+                HttpRequestMessage requestMessage = new HttpRequestMessage();
+                var response = await client.GetAsync("api/campaign");
+                responseString = await response.Content.ReadAsStringAsync();
+            }
+            return responseString;
+        }
+    }
+}
---
+++ DonateKart_TestAPI/Models/CampaignDetail.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DonateKart_TestAPI.Models
+{
+    public class CampaignDetail
+    {
+         public int id { get; set; }
+        public string code { get; set; }
+        public string title { get; set; }
+        public bool featured { get; set; }
+        public int priority { get; set; }
+        public string campaignType { get; set; }
+        public string shortDesc { get; set; }
+        public string imageSrc { get; set; }
+        public DateTime created { get; set; }
+        public DateTime endDate { get; set; }
+        public double totalAmount { get; set; }
+        public double procuredAmount { get; set; }
+        public double totalProcured { get; set; }
+        public double backersCount { get; set; }
+        public int categoryId { get; set; }
+        public string location { get; set; }
+        public string ngoCode { get; set; }
+        public string ngoName { get; set; }
+        public int daysLeft { get; set; }
+        public double percentage { get; set; }
+    }
+    public class ResponseCampaignDetail
+    {
+        public string title { get; set; }
+        public double totalAmount { get; set; }
+        public double backersCount { get; set; }
+        public DateTime endDate { get; set; }
+
+    }
+}
